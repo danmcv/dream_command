5a1fcfa4f25a91221d75acceb2864f64fd4d45e5
danmcv <danmcv5@gmail.com>
2016-11-02 12:51:22 -0700

    Rango app now links to about page, and back home

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
+from rango import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    # maps any URLS starting with rango/ to be 
+    # handled by the rango application
+    url(r'^rango/', include('rango.urls')),
     url(r'^admin/', admin.site.urls),
 ]
